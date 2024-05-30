76a202185eb67398f3569f07606988c8e6b1639c
Kruthay Kumar Reddy Donapati <kruthay@gmail.com>
2024-05-30 17:11:15 -0400

    Initial Commit

---
+++ FaceTimeClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceTimeClone
+//
+//  Created by Kruthay Kumar Reddy Donapati on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceTimeClone/FaceTimeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceTimeCloneApp.swift
+//  FaceTimeClone
+//
+//  Created by Kruthay Kumar Reddy Donapati on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceTimeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
